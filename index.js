225d41ced2eb94b901e7f46dda2bc6fc7c7f56bb
JakubGitC <129523625+JakubGitC@users.noreply.github.com>
2024-10-07 23:45:40 +0200

    Update index.js
     Arrays and Objects converter

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function celsiusToFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32;
 }
+//Convert Arrays and Objects
+function arrayToObject(arr, keyField) {
+    return arr.reduce((obj, item) => {
+        obj[item[keyField]] = item;
+        return obj;
+    }, {});
+}
